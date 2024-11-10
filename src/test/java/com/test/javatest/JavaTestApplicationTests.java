bf5bb7bee9d8235cabc6e8fa36f3a2a3dd7184f2
notitle12 <notitle1215@gmail.com>
2024-11-10 21:26:04 +0900

    release: 배포를 위해 서비스를 도커이미지화

--- src/test/java/com/test/javatest/JavaTestApplicationTests.java
+++ src/test/java/com/test/javatest/JavaTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.test.javatest;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavaTestApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.test.javatest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JavaTestApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
